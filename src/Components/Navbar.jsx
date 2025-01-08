fbf08318a47f3fd8b82b285d43ea241cb2f55486
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2025-01-08 12:02:30 +0530

    Added With Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="relative">
+        <img
+          className="h-9 rounded-full mt-4 mx-2 items-center"
+          src="https://akm-img-a-in.tosshub.com/indiatoday/images/media_bank/202307/why-design-thinking-is-important-in-todays-job-industries-132433-16x9.jpg?VersionId=r3DkuIlkjSZI0KClaN79ksVBKui34xNG&size=690:388"
+        />
+        <h1 className="absolute top-0 mx-24 mt-2 items-center text-black font-bold ">
+          Greata{" "}
+        </h1>
+        <h1 className="absolute top-0 mx-40 mt-2 items-center">Mae Events</h1>
+      </div>
+      <nav>
+        <ul className="flex justify-end gap-16 absolute top-0 right-1 mt-4 items-center font-bold  ">
+          <li className="hover:text-orange-600">Home</li>
+          <li className="hover:text-orange-600">About Me</li>
+          <li className="hover:text-orange-600">My Portfolio</li>
+          <li className="hover:text-orange-600">Testimonials</li>
+          <li className="hover:text-orange-600">Contract</li>
+
+          <button className=" flex border bg-black text-white px-7 rounded-2xl items-center justify-start gap-2 p-2">
+            <span >Work With Me</span>
+            <BsArrowUpRightCircleFill  className="ml-2 text-2xl text-orange-600  hover:text-yellow-400 " />
+          </button>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
